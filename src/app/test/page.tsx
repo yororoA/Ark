f9e47bbebbaf216e507c0df5098a4ffe4b5bde50
yororoA <3364817735@qq.com>
2026-07-18 13:31:36 +0800

    chore: 调整测试页展示组件

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -14,10 +14,10 @@ export default function Test() {
   return (
     <div className='relative'>
       {/* <Loading type="charJump" text="Loading..." /> */}
-      {/* <Loading type="animation" text="正在提交反馈至神经......" /> */}
+      <Loading type="animation" text="正在提交反馈至神经......" />
       {/* <Loading type="text" text="加载中..." /> */}
       {/* <Button size="large" onClick={() => console.log('提交')}>账号管理</Button> */}
-      <Input label="账号" id='account' placeholder="请输入账号" />
+      {/* <Input label="账号" id='account' placeholder="请输入账号" /> */}
     </div>
   );
 }
